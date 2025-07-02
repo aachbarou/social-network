ad6a6322cbc071bf1df598bafbcde8aa4aec1912
ahmed achbarou <ahmedachbarou842@gmail.com>
2025-07-02 02:30:34 +0100

    fix(validation): Align date parsing with YYYY-MM-DD format

--- backend/pkg/utils/validation.go
+++ backend/pkg/utils/validation.go
@@ -2,6 +2,10 @@ package utils
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"time"
+
 	"social-network/pkg/models"
 )
 
@@ -20,37 +24,59 @@ func ValidateNewUser(user models.User) error {
 		return err
 	}
 	if err := validateEmail(user.Email); err != nil {
+		fmt.Println("Email validation failed: %w", err)
 		return err
 	}
 	return nil
 }
+
 func validateFirstName(name string) error {
 	if fieldEmpty(name) {
 		return errors.New("Validation error")
 	}
 	return nil
 }
+
 func validateLastName(name string) error {
 	if fieldEmpty(name) {
 		return errors.New("Validation error")
 	}
 	return nil
 }
+
 func validateBirth(birthDate string) error {
 	if fieldEmpty(birthDate) {
 		return errors.New("Validation error")
 	}
+	if err := validateDate(birthDate); err != nil {
+		return fmt.Errorf("Invalid date format: %w", err)
+	}
 	return nil
 }
+
 func validatePassword(password string) error {
 	if fieldEmpty(password) {
 		return errors.New("Validation error")
 	}
 	return nil
 }
+
 func validateEmail(email string) error {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
 	if fieldEmpty(email) {
-		return errors.New("Validation error")
+		return errors.New("Email cannot be empty")
+	}
+	if !emailRegex.MatchString(email) {
+		return errors.New("Invalid email format")
+	}
+	return nil
+}
+
+func validateDate(dateStr string) error {
+	layout := "2006-01-02"
+	_, err := time.Parse(layout, dateStr)
+	if err != nil {
+		return fmt.Errorf("invalid date format: %s", err)
 	}
 	return nil
 }
