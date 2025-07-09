7ea9771542460908a953b435040762b7f091f7f3
ahmed achbarou <ahmedachbarou842@gmail.com>
2025-07-09 11:31:35 +0100

    broadcasted via WebSocket to all connected clients for every user involved

--- backend/pkg/handlers/messages.go
+++ backend/pkg/handlers/messages.go
@@ -84,10 +84,7 @@ func (handler *Handler) Messages(w http.ResponseWriter, r *http.Request) {
 	utils.RespondWithMessages(w, messages, 200)
 }
 
-// new chat message wits for POST requet with SENDER, RECEIVER AND TYPE
 // function saves new message and responds
-// to sender with regular http response
-// to recievers through websocket connection
 func (handler *Handler) NewMessage(wsServer *ws.Server, w http.ResponseWriter, r *http.Request) {
 	w = utils.ConfigHeader(w)
 	/* --------------------------- read incoming data --------------------------- */
@@ -101,9 +98,8 @@ func (handler *Handler) NewMessage(wsServer *ws.Server, w http.ResponseWriter, r
 	/* -------------------- attach sender id ------------------------------------ */
 	msg.SenderId = r.Context().Value(utils.UserKey).(string)
 
-	var newChatFlag = "" //flag is raised with valu "NEW" if tha chat does not exist for user yet
+	var newChatFlag = "" 
 
-	// check if receiver is following current user
 	isFollowingBack, err := handler.repos.UserRepo.IsFollowing(msg.SenderId, msg.ReceiverId)
 	if err != nil {
 		utils.RespondWithError(w, "Error on saving checking status", 200)
@@ -170,7 +166,6 @@ func (handler *Handler) NewMessage(wsServer *ws.Server, w http.ResponseWriter, r
 			newChatFlag = "NEW"
 		}
 	}
-	/* --------------------------- generate message id -------------------------- */
 	msg.ID = utils.UniqueId()
 	/* ---------------------------- save in database ---------------------------- */
 	err = handler.repos.MsgRepo.Save(msg)
@@ -183,48 +178,50 @@ func (handler *Handler) NewMessage(wsServer *ws.Server, w http.ResponseWriter, r
 	}
 	/* --------------------------- attach sender  info -------------------------- */
 	msg.Sender, _ = handler.repos.UserRepo.GetDataMin(msg.SenderId)
-	// send message respond with new message to sender
-	utils.RespondWithMessages(w, []models.ChatMessage{msg}, 200)
+	// The new message will be sent via WebSocket to ensure all tabs are updated.
+	utils.RespondWithSuccess(w, "Message sent successfully", 200)
 
-	/* ------------------ respond through websocket to receiver ----------------- */
+	/* ------------------ respond through websocket to all parties ----------------- */
 	if msg.Type == "PERSON" {
 		for client := range wsServer.Clients {
-			if client.ID == msg.ReceiverId {
+			if client.ID == msg.SenderId || client.ID == msg.ReceiverId {
 				client.SendChatMessage(msg, newChatFlag)
 			}
 		}
-	} else if msg.Type == "GROUP" { //incase of group find and respond to all recievers
-		// find all group members + admin except the sender
+	} else if msg.Type == "GROUP" { // In case of a group, find and respond to all members.
 		allMembers, err := handler.repos.GroupRepo.GetMembers(msg.ReceiverId)
 		if err != nil {
-			utils.RespondWithError(w, "Error on geting group members", 200)
+			fmt.Println("Error on getting group members:", err)
 			return
 		}
-		for i := 0; i < len(allMembers); i++ {
-			if allMembers[i].ID != msg.SenderId {
-				/* -------- save also in group_messages table -------- */
-				err = handler.repos.MsgRepo.SaveGroupMsg(models.ChatMessage{ID: msg.ID, ReceiverId: allMembers[i].ID})
-			}
-			for client := range wsServer.Clients {
-				if client.ID == msg.SenderId {
-					continue
-				}
-				if client.ID == allMembers[i].ID {
-					client.SendChatMessage(msg, "")
-				}
-			}
 
+		memberIDs := make(map[string]bool)
+		for _, member := range allMembers {
+			memberIDs[member.ID] = true
+		}
+
+		for _, member := range allMembers {
+			if member.ID != msg.SenderId {
+				err = handler.repos.MsgRepo.SaveGroupMsg(models.ChatMessage{ID: msg.ID, ReceiverId: member.ID})
+				if err != nil {
+					fmt.Printf("Error saving group message read status for user %s: %v\n", member.ID, err)
+				}
+			}
+		}
+		for client := range wsServer.Clients {
+			if memberIDs[client.ID] {
+				client.SendChatMessage(msg, "")
+			}
 		}
 	}
 }
 
+
 // respond with list of messages, that user has missed
-// in response include message_id, senderId (group id or user id) type -> group or person
 func (handler *Handler) UnreadMessages(w http.ResponseWriter, r *http.Request) {
 	w = utils.ConfigHeader(w)
 
 	userId := r.Context().Value(utils.UserKey).(string)
-	/* ---------- collect chat stats from db for prvate and group chats --------- */
 	messages, err := handler.repos.MsgRepo.GetUnread(userId)
 	if err != nil {
 		utils.RespondWithError(w, "Error on getting the unread messages", 200)
@@ -282,7 +279,6 @@ func (handler *Handler) ResponseChatRequest(w http.ResponseWriter, r *http.Reque
 		return
 	}
 	/* ---------------------------- read incoming data --------------------------- */
-	// Try to decode the JSON request to a new response
 	type Response struct {
 		RequestID string `json:"requestId"`
 		Response  string `json:"response"` // ACCEPT or DECLINE
